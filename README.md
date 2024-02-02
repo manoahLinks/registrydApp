77b8005bc60e9a31a34ec21e849ecdeb595f496b
manoahLinks <manoahluka@gmail.com>
2024-02-02 12:27:05 +0100

    testing & readme

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
 Regitry dApp
 
+## Overview
+Registry dApp is a simple decentralized application that keep tracks of user information, it has 2 state variables namely; entityName and entityage. This dApp provides functions that aid us get entity details and also state changing functions to make changes to the state variables listed above.
+
+
+## contract functions
+
+updateName: This function accepts one argument which is of type string, its updates the entityName state variable to the value passed in the function.
+
+updateAge: This function accepts one argument which is of type uint, it updates the entityAge state variable to the value passed in the function.
+
+getEntityDetails: This is function is a view function, it returns the state variables namely; entityName and entityAge as tuple.
+
+## Tools
+node.js version 18
+hardhat
+
+## set up
+
+git clone ``````
+
+## frontend setup
+
+cd frontend
+npm install
+npm run dev
+
+# contract
+
+cd contracts
+npm install
+
+compile: npx hardhat compile
+test: npx hardhat test test/registry.ts  
+
+
+## contract
+solidity
+
+## frontend
+react with vite
+tailwindcss
+ethers.js
+
+## test
+chai
+
+network : sepolia
 contract Address : 0xF248e7686A629ce7cC5515e28d8b4E31bed40B93
--- contracts/test/registry.ts
+++ contracts/test/registry.ts
@@ -19,9 +19,47 @@ import {
     }
 
     describe('deployment', () => {
-        it('should confirm constructor variables on deployment', async () => {
+        it('should confirm constructor args are equal to number passed', async () => {
+            const {registry, name, age} = await loadFixture(deployRegistryContract);
+            expect((await registry.getEntityDetails()).length).to.equal([name, BigInt(age)].length)
+        })
+
+        it('should confirm constructor name are same as deployed', async () => {
+            const {registry, name} = await loadFixture(deployRegistryContract);
+            expect((await registry.getEntityDetails())[0]).to.equal(name);
+        })
+
+        it('should confirm constructor Age are same as deployed', async () => {
+            const {registry, age} = await loadFixture(deployRegistryContract);
+            expect((await registry.getEntityDetails())[1]).to.equal(age);
+        })
+    })
+
+    describe('update name', () => {
+        it('should confirm if new name has been updated', async () => {
             const {registry} = await loadFixture(deployRegistryContract);
-            
+            await registry.updateName('Jonzing man')
+            expect((await registry.getEntityDetails())[0]).to.equal('Jonzing man');
+        })
+
+        it('should confirm if new deployed age has been updated after func call', async () => {
+            const {registry, name} = await loadFixture(deployRegistryContract);
+            await registry.updateName('Jonzing man')
+            expect((await registry.getEntityDetails())[0]).to.not.equal(name);
+        })
+    })
+
+    describe('update Age', () => {
+        it('should confirm if new Age has been updated', async () => {
+            const {registry} = await loadFixture(deployRegistryContract);
+            await registry.updateAge(40)
+            expect((await registry.getEntityDetails())[1]).to.equal(40);
+        })
+
+        it('should confirm if new deployed age has been updated after func call', async () => {
+            const {registry, age} = await loadFixture(deployRegistryContract);
+            await registry.updateAge(40)
+            expect((await registry.getEntityDetails())[1]).to.not.equal(age);
         })
     })
   })
